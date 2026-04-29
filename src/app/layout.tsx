1aa4fddd1fffbe5334f7896057ef61f475f10aab
medicointernista <localrankmedellin@gmail.com>
2026-04-29 12:55:41 -0500

    Set base font and remove font imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import Script from "next/script";
 import "./globals.css";
 import { VisualEditsMessenger } from "orchids-visual-edits";
-
-const geistSans = localFont({
-  src: "../../public/fonts/geist-latin.woff2",
-  variable: "--font-geist-sans",
-  display: "swap",
-});
-
-const geistMono = localFont({
-  src: "../../public/fonts/geist-mono-latin.woff2",
-  variable: "--font-geist-mono",
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Gastritis y Colon Irritable en Medellín - Dr. Willmer Obregón",
@@ -41,7 +28,7 @@ export default function RootLayout({
               "telephone": "+573053979628",
               "address": {
                 "@type": "PostalAddress",
-                  "streetAddress": "Biencrecer Cra 81 #27-17 Consultorio 1, Belén La Palma",
+                "streetAddress": "Biencrecer Cra 81 #27-17 Consultorio 1, Belén La Palma",
                 "addressLocality": "Medellín",
                 "addressRegion": "Antioquia",
                 "postalCode": "05001",
@@ -56,7 +43,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="antialiased">
         <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
